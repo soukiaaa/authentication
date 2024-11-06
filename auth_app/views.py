d3f7843be8913cf8768268d7379586cb8b8eb110
soukiaaa <soukimohamed@gmail.com>
2024-11-06 14:15:47 +0100

    main

---
+++ auth_app/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from .form import CustomUserCreationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def inscription(request):
+    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('connexion')
+    return render(request, 'inscription.html', {'form': form})
+
+def connexion(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('acceuil')
+        else:
+            messages.error(request, 'Nom d\'utilisateur ou mot de passe incorrect.')
+    return render(request, 'connexion.html')
+
+@login_required
+def acceuil(request):
+    return render(request, 'acceuil.html')
+
+def deconnexion(request):
+    logout(request)
+    return redirect('connexion')
